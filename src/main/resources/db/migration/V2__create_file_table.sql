c8e381de7c4dbef9e9716d8625527f82f4cdfab7
gabriel <byeh997@gmail.com>
2024-09-28 16:12:31 -0300

    adicionado campo de conteúdo no arquivo

--- src/main/resources/db/migration/V2__create_file_table.sql
+++ src/main/resources/db/migration/V2__create_file_table.sql
@@ -2,5 +2,6 @@ create table file(
     id bigint not null primary key,
     name varchar(100) not null,
     parent_folder_id bigint not null,
+    file_content text,
     foreign KEY (parent_folder_id) references folder(id)
 );
