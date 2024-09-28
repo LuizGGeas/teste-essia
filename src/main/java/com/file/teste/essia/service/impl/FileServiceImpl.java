e8daab275f9ab21112a1a2b70ae49b2c6bf15796
gabriel <byeh997@gmail.com>
2024-09-28 16:10:53 -0300

    adicionado validação para não salvar com pasta nula e nome inválido

--- src/main/java/com/file/teste/essia/service/impl/FileServiceImpl.java
+++ src/main/java/com/file/teste/essia/service/impl/FileServiceImpl.java
@@ -1,10 +1,25 @@
 package com.file.teste.essia.service.impl;
 
+import com.file.teste.essia.exception.ErrorCode;
+import com.file.teste.essia.exception.NotValidParamsException;
 import com.file.teste.essia.model.File;
 import com.file.teste.essia.repository.FileRepository;
 import com.file.teste.essia.service.FileService;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class FileServiceImpl extends AbstractServiceImpl<FileRepository, File> implements FileService  {
+    @Override
+    public File save(File entity) {
+        if(Objects.isNull(entity.getSourceFolder()))
+            throw new NotValidParamsException("Pasta precisa de uma raiz", ErrorCode.INVALID_PARAMS);
+
+        if(Strings.isBlank(entity.getName()))
+            throw new NotValidParamsException("Pasta precisa de um nome válido", ErrorCode.INVALID_PARAMS);
+
+        return super.save(entity);
+    }
 }
--- src/main/java/com/file/teste/essia/service/impl/FolderServiceImpl.java
+++ src/main/java/com/file/teste/essia/service/impl/FolderServiceImpl.java
@@ -1,10 +1,26 @@
 package com.file.teste.essia.service.impl;
 
+import com.file.teste.essia.exception.ErrorCode;
+import com.file.teste.essia.exception.NotValidParamsException;
 import com.file.teste.essia.model.Folder;
 import com.file.teste.essia.repository.FolderRepository;
 import com.file.teste.essia.service.FolderService;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class FolderServiceImpl extends AbstractServiceImpl<FolderRepository, Folder> implements FolderService {
+
+    @Override
+    public Folder save(Folder entity) {
+        if(Objects.isNull(entity.getParentFolder()))
+            throw new NotValidParamsException("Pasta precisa de uma raiz", ErrorCode.INVALID_PARAMS);
+
+        if(Strings.isBlank(entity.getName()))
+            throw new NotValidParamsException("Pasta precisa de um nome válido", ErrorCode.INVALID_PARAMS);
+
+        return super.save(entity);
+    }
 }
